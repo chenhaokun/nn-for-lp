72f28475753aa58bb67270f26faa555a95247937
chenhaokun <2536221273@qq.com>
2017-03-17 16:59:10 +0800

    net1

--- data_process/divide_data.py
+++ data_process/divide_data.py
@@ -1,24 +1,61 @@
 #coding:utf-8
 import random
 
-def randomly_divide_data(source_file_path,target_file_path1,target_file_path2,weight1,weight2):
+def randomly_divide_data(source_file_path,target_file_path1,target_file_path2,tt_rate,np_rate,node_num):
+    n_edge_dict={}
+    n_edge_list=[]
+    p_edge_count=0
+    n_edge_count=0
+    print('step1...')
+    with open(source_file_path,'r') as source_file:
+	for line in source_file:
+	    tmpstr=line[0:len(line)-1]
+	    n_edge_dict[tmpstr]=''
+	    p_edge_count+=1
+    n_edge_num=np_rate*p_edge_count
+    print('step2...')
+    while 1:
+	line='%d\t%d'%(random.randint(1,node_num),random.randint(1,node_num))
+	if line not in n_edge_dict:
+	    n_edge_list.append(line+'\t%d\n'%0)
+            n_edge_count+=1
+    	    if n_edge_count==n_edge_num:
+		break
+    print('step3...')    
     with open(source_file_path,'r') as source_file,open(target_file_path1,'w') as target_file1,open(target_file_path2,'w') as target_file2:
 	list1=[]
 	list2=[]
 	count1=0
 	count2=0
-	threshold=weight1/float(weight1+weight2)
+	n_index=0
+	threshold=tt_rate/float(tt_rate+1)
 	for line in source_file:
 	    if random.random()<threshold:
-		list1.append(line)
+		list1.append(line[0:len(line)-1]+'\t%d\n'%1)
 		count1+=1
+		for i in range(np_rate):
+		    list1.append(n_edge_list[n_index])
+		    n_index+=1
 	    else:
-		list2.append(line)
+		list2.append(line[0:len(line)-1]+'\t%d\n'%1)
 		count2+=1
+		for i in range(np_rate):
+		    list2.append(n_edge_list[n_index])
+		    n_index+=1	
 	print count1
 	print count2
 	target_file1.writelines(list1)
 	target_file2.writelines(list2)
 
+def tsv_to_csv(source_file_path,target_file_path):
+    with open(source_file_path,'r') as source_file,open(target_file_path,'w') as target_file:
+	target_list=[]
+	for line in source_file:
+	    newline=','.join(line.split('\t'))
+	    target_list.append(newline)
+	target_file.writelines(target_list)	     
+
 if __name__=='__main__':
-    randomly_divide_data('../../data/soc-pokec-relationships.txt','../../data/train_edges','../../data/test_edges',8,2)
+    #randomly_divide_data('../../data/raw/soc-pokec-relationships.txt','../../data/data/train_data','../../data/data/test_data',4,4,1632803)
+    tsv_to_csv('../../data/data/train_data','../../data/data/train_data_csv')
+    tsv_to_csv('../../data/data/test_data','../../data/data/test_data_csv')
---
+++ train/nn1.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import numpy as np
+
+def get_input_tensors(input_file_path):
+    list1=[]
+    list2=[]
+    list3=[]
+    with open(input_file_path,'r') as input_file:
+	for line in input_file:
+	    items=line[0:len(line)-1].split('\t')
+	    list1.append(int(items[0]))
+	    list2.append(int(items[1]))
+	    list3.append(int(items[2]))
+    return [tf.convert_to_tensor(list1),tf.convert_to_tensor(list2),tf.convert_to_tensor(list3)]
+
+def shuffle_batch_test():
+    filenames=['../../data/test/train_data_csv_test']
+    filename_queue=tf.train.string_input_producer(filenames)
+    reader=tf.TextLineReader()
+    key,value=reader.read(filename_queue)
+    u1,u2,label=tf.decode_csv(value,record_defaults=[[0],[0],[0.0]])
+    u1_batch,u2_batch,label_batch=tf.train.shuffle_batch([u1,u2,label],batch_size=1,capacity=20,min_after_dequeue=10)
+    with tf.Session() as sess:
+        coord=tf.train.Coordinator()
+        thread=tf.train.start_queue_runners(coord=coord)
+        for i in range(40):
+            u1val,u2val,lval=sess.run([u1_batch,u2_batch,label_batch])
+	    print u1val,u2val,lval
+        coord.request_stop()
+        coord.join(thread)
+
+def train_model(input_file_path,params):
+    filenames=[input_file_path]
+    filename_queue=tf.train.string_input_producer(filenames)
+    reader=tf.TextLineReader()
+    key,value=reader.read(filename_queue)
+    u1,u2,y=tf.decode_csv(value,record_defaults=[[0],[0],[0.0]])
+
+    u1s,u2s,ys=tf.train.shuffle_batch([u1,u2,y],batch_size=params['batch_size'],capacity=3000,min_after_dequeue=1000)
+
+    #u1s=tf.placeholder(tf.int32,shape=[None,])
+    #u2s=tf.placeholder(tf.int32,shape=[None,])
+    #ys=tf.placeholder(tf.float32,shape=[None,])
+    oh_u1s=tf.one_hot(u1s-tf.ones_like(u1s),depth=params['node_num'])
+    oh_u2s=tf.one_hot(u2s-tf.ones_like(u2s),depth=params['node_num'])
+
+    weights1_1=tf.Variable(tf.ones([params['node_num'],params['h_len']]))
+    weights1_2=tf.Variable(tf.ones([params['node_num'],params['h_len']]))
+    biases1=tf.Variable(tf.zeros([params['h_len']]))
+    h1=tf.nn.softmax(tf.matmul(oh_u1s,weights1_1)+tf.matmul(oh_u2s,weights1_2)+biases1)
+    
+    weights2=tf.Variable(tf.ones([params['h_len'],params['h_len']]))
+    biases2=tf.Variable(tf.zeros([params['h_len']]))
+    h2=tf.nn.softmax(tf.matmul(h1,weights2)+biases2)
+
+    weights3=tf.Variable(tf.ones([params['h_len'],1]))
+    biases3=tf.Variable(tf.zeros([1]))
+    ys_=tf.sigmoid(tf.matmul(h2,weights3)+biases3)
+
+    cross_entropy=-tf.reduce_sum(ys*tf.log(ys_))
+
+    train_step=tf.train.GradientDescentOptimizer(params['learning_rate']).minimize(cross_entropy)
+
+    with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+        coord=tf.train.Coordinator()
+        thread=tf.train.start_queue_runners(coord=coord)
+	for i in range(int(float(params['round'])*params['edge_num']/params['batch_size'])):
+	    train_step.run()
+	    if i%100==0:
+	        print 'step count: %db'%(i/100)
+	coord.request_stop()
+        coord.join(thread)
+
+def main():
+    input_file_path='../../data/data/train_data_csv'
+    params={'node_num':1632803,'edge_num':30622564,'round':20,'learning_rate':0.01,'batch_size':128,'h_len':10}
+    train_model(input_file_path,params)
+
+if __name__=='__main__':
+    #shuffle_batch_test()
+    main()
